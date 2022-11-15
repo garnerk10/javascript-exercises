b6a0f42b9e0756e052a70e41a381e2b050d069a7
Kyle Garner <garnerk10@gmail.com>
2022-11-15 15:44:10 -0500

    completed reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = str => {
+    let newString = '';
 
+    for  (let i = str.length - 1; i >= 0; i--){
+        newString += str[i]
+    };
+
+    return newString
 };
 
+console.log(reverseString('What is up my dude'))
 // Do not edit below this line
 module.exports = reverseString;
