83af9c0fd5d8d01424f149d5a7cfa6399532f68a
Kyle Garner <garnerk10@gmail.com>
2022-11-15 15:19:32 -0500

    Completed repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = (string, num) => {
+    let newString = "";
+    for(let i = 0; i < num; i++){
+        newString += string
+    };
 
+    return newString;
 };
 
+console.log(repeatString('hey', 10))
 // Do not edit below this line
 module.exports = repeatString;
