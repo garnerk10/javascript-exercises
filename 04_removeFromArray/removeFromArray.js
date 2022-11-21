45eb1db6b4c246f886ffb5385764ca3c0de0974b
Kyle Garner <garnerk10@gmail.com>
2022-11-20 20:48:12 -0500

    completed removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...deletes) {
+    for (const num of deletes){
+        for (let i = 0; i < arr.length; i++){
+            if(arr[i] === num){
+                arr.splice(i, 1);
+                i--;
+            }
+        };
+    }
+    return arr
 };
+
+//let testArr = [1, 2, 3, 4, 5, 6];
+
+//console.log(removeFromArray(testArr, 3, 5, 7));
 
 // Do not edit below this line
 module.exports = removeFromArray;
