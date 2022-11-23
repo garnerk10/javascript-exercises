0f3b1f80dc26ec2f06b6b36ebb1c0dbf24bd3c60
Kyle Garner <garnerk10@gmail.com>
2022-11-22 22:02:41 -0500

    completed tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(fahr) {
+  let newCTemp = ((fahr - 32) * (5/9));
+  let roundCTemp = Number(newCTemp.toFixed(1));
 
+  return roundCTemp
 };
 
-const ctof = function() {
+const ctof = function(cels) {
+  let newFTemp = (cels * (9/5) + 32);
+  let roundFTemp = Number(newFTemp.toFixed(1));
 
+  return roundFTemp
 };
 
 // Do not edit below this line
