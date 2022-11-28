3320ba039a754f91a30d84e8d8c9bc6167ea2437
Kyle Garner <garnerk10@gmail.com>
2022-11-28 15:17:41 -0500

    completed palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let lowerCaseStr = str.toLowerCase();
+    let strToCheck = "";
+    let newStr = "";
+
+    for(let i = 0; i < str.length; i++){
+        if(lowerCaseStr.charCodeAt(i) >= 97 && lowerCaseStr.charCodeAt(i) <= 122){
+            strToCheck += lowerCaseStr[i]}
+    };
+
+    for(let j = str.length - 1; j >= 0; j--){
+        if(lowerCaseStr.charCodeAt(j) >= 97 && lowerCaseStr.charCodeAt(j) <= 122){
+            newStr += lowerCaseStr[j]}
+    };
+
+    if(newStr === strToCheck){
+        return true
+    }
+    else {return false};
+
 
 };
 
