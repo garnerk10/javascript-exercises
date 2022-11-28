e830fddda38886734435601226d3dcfd70b4beba
Kyle Garner <garnerk10@gmail.com>
2022-11-28 16:11:36 -0500

    completed fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibSequence = [1, 1, 2];
+    let getNum = Number(num);
+
+    if(getNum > 0){
+    for(let i = 2; i <= getNum; i++){
+        newAdd = (fibSequence[i-1] + fibSequence[i]);
+        fibSequence.push(newAdd);
+    };} else {
+        return 'OOPS'
+    };
+
+    return fibSequence[getNum - 1];
 
 };
-
+fibonacci(3);
 // Do not edit below this line
 module.exports = fibonacci;
