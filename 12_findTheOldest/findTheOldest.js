3feb038579ec4316edaf3ae03a2c89ebcba40a01
Kyle Garner <garnerk10@gmail.com>
2022-11-29 21:55:31 -0500

    completed findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let date = new Date();
+    let updatedYear = [];
 
+    arr.forEach(person => {
+        if('yearOfDeath' in person === false){
+            person.yearOfDeath = date.getFullYear();
+            updatedYear.push(person);
+        } else {
+            updatedYear.push(person);
+        }
+    });
+
+    const ordered = updatedYear.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1)
+
+    return ordered[0];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
